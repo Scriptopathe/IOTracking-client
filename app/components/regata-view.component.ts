215927918329b088caca22796acd9ecf8ac8ec31
Scriptopathe <josue.alvarez@hotmail.fr>
2016-12-09 00:26:55 +0100

    Create separate regata component

---
+++ app/components/regata-view.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, ViewChild, ElementRef } from '@angular/core';
+import { Http, Response }   from '@angular/http';
+import { Observable }       from 'rxjs/Observable';
+import { Regata, RegatasService } from '../services/regatas.service'
+import * as $ from "jquery";
+
+@Component({
+    selector: 'regata-view',
+    templateUrl: 'app/components/regata-view.template.html'
+})
+
+export class RegataViewComponent  { 
+    @Input('regata') regata: Regata;
+    @ViewChild("panel") panel : ElementRef;
+    constructor(private http : Http, private regataSvc : RegatasService) {        
+        
+    }
+
+    public onRemove()
+    {
+        let element : HTMLDivElement = this.panel.nativeElement
+        element.setAttribute('class', element.getAttribute('class') + ' animated zoomOutDown')
+        let othis = this
+        $(element).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() { 
+            othis.regataSvc.deleteRegata(othis.regata) 
+        });
+    }
+}
