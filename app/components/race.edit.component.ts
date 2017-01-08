8ea946ad7c4765aecc702d451abe94087393d490
CecileDuthoit <cecile.duthoit@gmail.com>
2017-01-08 17:02:47 +0100

    add racer edition - in progress

--- app/components/race.edit.component.ts
+++ app/components/race.edit.component.ts
@@ -4,7 +4,7 @@ import { Http, Response }                           from '@angular/http';
 import { Observable }                               from 'rxjs/Observable';
 import { RegatasService }                           from '../services/regatas.service'
 import { Regata, Race}                              from '../services/server-model'
-import * as $ from 'jquery'
+import * as $                                       from 'jquery'
 
 @Component({
     selector: 'race-edit',
@@ -40,12 +40,9 @@ export class RaceEditionComponent  {
                 this.regataId = params['regata']
                 this.regataSvc.findById(this.regataId).subscribe((regata : Regata) => {
                     this.currentRegata = regata
+                    this.currentRace = this.currentRegata.races[this.indexRace]
                 })
-                this.currentRace = this.currentRegata.races[this.indexRace]
-                this.modalId = this.currentRegata.name + "_" + this.currentRace.name
+                //this.modalId = this.currentRegata.name + "_" + this.currentRace.name
         });
-
-        //$(this.modalId).modal();
-    }
+     }
 }
-
