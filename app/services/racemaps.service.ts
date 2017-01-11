f31f7b4f822270d2c542fe0fcadb7c4d0e6534da
Scriptopathe <josue.alvarez@hotmail.fr>
2017-01-11 17:42:20 +0100

    Add racemaps service

---
+++ app/services/racemaps.service.ts
@@ -0,0 +1,75 @@
+import { Injectable }                   from '@angular/core';
+import { Http, Response }               from '@angular/http';
+import { Observable, Subscriber }       from 'rxjs/Rx';
+import { Inject }                       from '@angular/core';
+import { RaceMap, Server }              from './server-model';
+
+@Injectable()
+export class RacemapsService {
+    public constructor(private http : Http) {
+        
+    }
+
+
+    public deleteRacemap(racemap : RaceMap) : Observable<boolean> {
+        var self = this
+        
+        return new Observable<boolean>((subscriber : Subscriber<boolean>) => {
+            if(racemap.identifier == null) {
+                // new racemap
+                subscriber.next(true)
+                subscriber.complete()
+            } else {
+                // update racemap
+                self.http.delete(Server.RaceMapUrl + "/" + racemap.identifier)
+                .subscribe((value) => {
+                    if(!value.ok) subscriber.error(value.status)
+                    subscriber.next(value.ok)
+                    subscriber.complete()
+                })
+            }
+        })
+    }
+
+
+
+    public saveRacemap(racemap : RaceMap) : Observable<boolean> {
+        var self = this
+        return new Observable<boolean>((subscriber : Subscriber<boolean>) => {
+            if(racemap.identifier == null) {
+                // new racemap
+                self.http.post(Server.RaceMapUrl, JSON.stringify(racemap))
+                .subscribe((value) => {
+                    if(!value.ok) subscriber.error(value.status)
+                    subscriber.next(value.ok)
+                    subscriber.complete()
+                })
+
+            } else {
+                // update racemap
+                self.http.put(Server.RaceMapUrl + "/" + racemap.identifier, 
+                              JSON.stringify(racemap)).subscribe((value) => {
+                    if(!value.ok) subscriber.error(value.status)
+                    subscriber.next(value.ok)
+                    subscriber.complete()
+                })
+            }
+        })
+    }
+
+    public loadRacemaps() : Observable<RaceMap[]> {
+        var self = this
+        return new Observable<RaceMap[]>((subscriber : Subscriber<RaceMap[]>) => {
+            self.http.get(Server.RaceMapUrl).subscribe((value) => {
+                var maps : RaceMap[] = []
+                var jsonMaps : any[] = value.json()
+                for(let jsonMap of jsonMaps) {
+                    var map = new RaceMap()
+                    maps.push(map.loadValues(jsonMap))
+                }
+                subscriber.next(maps)
+                subscriber.complete()
+            })
+        })
+    }
+}
